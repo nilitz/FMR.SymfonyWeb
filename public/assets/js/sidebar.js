2c2eec6e79498de2ca32f87c9d2c0a001dee3d28
nilitz <hugodegrossi@gmail.com>
2020-06-28 10:29:16 +0200

    Corrected : RentController modification & addition

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -2,8 +2,8 @@ function onSidebarButtonClick(){
 
     let sidebar = document.querySelector('div.sidebar')
     let sidebar_btn = document.querySelector('div.sidebar-btn')
-    sidebar.classList.toggle("visible")
-    sidebar_btn.classList.toggle("toggle")
+    sidebar.classList.toggle("sidebar-visible")
+    sidebar_btn.classList.toggle("btn-visible")
 }
 
 btn = document.querySelector('div.sidebar-btn')
--- src/Controller/Admin/AdminRentMachineController.php
+++ src/Controller/Admin/AdminRentMachineController.php
@@ -4,7 +4,7 @@
 namespace App\Controller\Admin;
 
 
-class AdminRentController
+class AdminRentMachineController
 {
 
 }
