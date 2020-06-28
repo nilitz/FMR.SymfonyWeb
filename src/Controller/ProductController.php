f25ab357047ce8cd4e80cc14620bed5d06058e08
nilitz <hugodegrossi@gmail.com>
2020-06-28 18:48:46 +0200

    Added : Product Page, Responsive & Dynamic Card & Grid system

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,9 +4,44 @@
 namespace App\Controller;
 
 
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
+    /**
+     * AdminProductController constructor.
+     * @param ProductRepository $productRepository
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(ProductRepository $productRepository, EntityManagerInterface $entityManager)
+    {
+
+        $this->productRepository = $productRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/products", name="products.index")
+     * @return Response
+     */
+    public function index()
+    {
+        $products = $this->productRepository->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
 }
